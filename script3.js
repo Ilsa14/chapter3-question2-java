a52345522824227c41b6632908b591544e0e9deb
Ilsa <s.ilsanaqvi@gmail.com>
2024-08-15 16:02:50 +0500

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,3 @@
+const birthYear = 2004;
+const message = `My birth year is ${birthYear}. Data type of declared variable is ${typeof birthYear}.`;
+document.getElementById('birthYearMessage').textContent = message;
